483a670ead6771ad92eabe52f6fd69a1a546bab3
RFoss5 <Riley.foss@trojans.dsu.edu>
2026-04-25 12:20:29 -0500

    Add project files.

---
+++ crop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace stardewCalendar
+{
+	internal class Crop : Item
+	{
+		Program.season season;
+
+		public Crop(string name,int sellPrice,int count, Program.season season)
+		{
+			this.season = season;
+			this.sellPrice = sellPrice;
+			this.count = count;
+			this.name = name;
+		}
+	}
+}
